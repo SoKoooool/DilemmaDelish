b2616622f6f91b5a0d3d636521578aaf2e52a2b7
SooJi <sokoooool86@gmail.com>
2023-09-25 10:36:46 +0900

    :lipstick: Design: IngredientButton UI설정 추가

--- DilemmaDelish/DilemmaDelish/Pages/Views/IngredientButton.swift
+++ DilemmaDelish/DilemmaDelish/Pages/Views/IngredientButton.swift
@@ -18,9 +18,50 @@ final class IngredientButton: UIButton {
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.config = .plain()
+        setupConfiguarance()
+        configuration = config
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        self.config = .plain()
+        setupConfiguarance()
+        configuration = config
+    }
+    
+    // MARK: - Life Cycle
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        configureRoundedImage()
+        configureTitle()
+    }
+    
+    // MARK: - Configurings
+    
+    private func setupConfiguarance() {
+        config.image = UIImage(systemName: "fork.knife.circle")
+        config.imagePadding = 4
+        config.baseForegroundColor = .darkText
+        config.background.backgroundColor = .systemPink.withAlphaComponent(0.2)
+        config.background.strokeColor = .systemPink
+        config.background.strokeWidth = 2
+        config.cornerStyle = .capsule
+        config.preferredSymbolConfigurationForImage = .init(scale: .large)
+    }
+    
+    private func configureRoundedImage() {
+        guard let imageView = imageView else { return }
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .white
+        imageView.layer.cornerRadius = imageView.bounds.width / 2
+        imageView.layer.borderWidth = 1
+        imageView.layer.borderColor = UIColor.white.cgColor
+    }
+    
+    private func configureTitle() {
+        guard let titleLabel = titleLabel else { return }
+        titleLabel.font = .systemFont(ofSize: 17, weight: .semibold)
+        titleLabel.numberOfLines = 1
     }
 }
