f077fd321eea8313fa8f89f61080b88a16da29bf
SooJi <sokoooool86@gmail.com>
2023-09-25 21:08:23 +0900

    :sparkles: Feat: 재료 목록에 필요한 도메인 모델 생성

---
+++ DilemmaDelish/DilemmaDelish/Domain/Models/IngredientItem.swift
@@ -0,0 +1,13 @@
+//
+//  IngredientItem.swift
+//  DilemmaDelish
+//
+//  Created by TORI on 2023/09/25.
+//
+
+import Foundation
+
+struct IngredientItem {
+    var name: String
+    var imageName: String
+}
