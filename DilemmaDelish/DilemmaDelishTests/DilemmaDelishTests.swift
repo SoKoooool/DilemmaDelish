8d9123a6ece0323af2ec69cfeb076558a1f56816
SooJi <sokoooool86@gmail.com>
2023-09-27 09:37:09 +0900

    :wrench: Chore: 유닛 테스트 생성

---
+++ DilemmaDelish/DilemmaDelishTests/DilemmaDelishTests.swift
@@ -0,0 +1,29 @@
+//
+//  DilemmaDelishTests.swift
+//  DilemmaDelishTests
+//
+//  Created by TORI on 2023/09/27.
+//
+
+import XCTest
+@testable import DilemmaDelish
+
+final class DilemmaDelishTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
