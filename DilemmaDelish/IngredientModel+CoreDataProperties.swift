c4642db02d92340675d2f53b438e40ababd8538f
SooJi <sokoooool86@gmail.com>
2023-10-10 10:40:44 +0900

    :wrench: Chore: NSManagedObject Entity 객체 생성

---
+++ DilemmaDelish/IngredientModel+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  IngredientModel+CoreDataProperties.swift
+//  DilemmaDelish
+//
+//  Created by TORI on 2023/10/10.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension IngredientModel {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<IngredientModel> {
+        return NSFetchRequest<IngredientModel>(entityName: "IngredientModel")
+    }
+
+    @NSManaged public var imageName: String?
+    @NSManaged public var name: String?
+
+}
+
+extension IngredientModel : Identifiable {
+
+}
