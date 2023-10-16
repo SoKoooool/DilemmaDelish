b986af60a55c88df94007d43cd1060b7c8f8702c
SooJi <sokoooool86@gmail.com>
2023-10-16 12:17:59 +0900

    :sparkles: Feat: Chat GPT API 객체 생성

---
+++ DilemmaDelish/DilemmaDelish/ChatCompletionsAPI.swift
@@ -0,0 +1,17 @@
+//
+//  ChatCompletionsAPI.swift
+//  DilemmaDelish
+//
+//  Created by TORI on 2023/10/16.
+//
+
+import Foundation
+
+final class ChatCompletionsAPI {
+    
+    private let apiKeys = ""
+    
+    func createChatCompletion() {
+        
+    }
+}
