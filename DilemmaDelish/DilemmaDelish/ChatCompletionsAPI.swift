7073062f82c9cf6018474641b5ea3ebd72621235
SooJi <sokoooool86@gmail.com>
2023-10-16 13:47:13 +0900

    :sparkles: Feat: api request 생성 기능 추가

--- DilemmaDelish/DilemmaDelish/ChatCompletionsAPI.swift
+++ DilemmaDelish/DilemmaDelish/ChatCompletionsAPI.swift
@@ -9,9 +9,20 @@ import Foundation
 
 final class ChatCompletionsAPI {
     
-    private let apiKeys = ""
+    private let apiKey = ""
     
     func createChatCompletion() {
         
+    }
+    
+    private func createChatRequest(with question: String) -> URLRequest {
+        let url = URL(string: GPTModel.gpt_3_5_turbo.endpoint)!
+        var request = URLRequest(url: url)
+        guard let requestBody = try? JSONSerialization.data(withJSONObject: ["question": question]) else { return }
+        
+        request.httpMethod = "POST"
+        request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+        request.httpBody = requestBody
+        return request
     }
 }
