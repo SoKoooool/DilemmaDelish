e8fd4db42902cc0b7a2f11f36a7d210771dac771
SooJi <sokoooool86@gmail.com>
2023-10-16 14:28:05 +0900

    :sparkles: Feat: post 요청 기능 생성

--- DilemmaDelish/DilemmaDelish/ChatCompletionsAPI.swift
+++ DilemmaDelish/DilemmaDelish/ChatCompletionsAPI.swift
@@ -11,16 +11,37 @@ final class ChatCompletionsAPI {
     
     private let apiKey = ""
     
-    func createChatCompletion() {
-        
+    func createChatCompletion(with request: URLRequest, completion: @escaping (Result<Data, NSError>) -> Void) {
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                completion(.failure(error as NSError))
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                let error = NSError(domain: "InvalidResponse", code: -2)
+                completion(.failure(error))
+                print(error._domain)
+                return
+            }
+            print("HTTP Status Code: \(httpResponse.statusCode)")
+            
+            guard let data = data else {
+                let error = NSError(domain: "NoData", code: -1)
+                completion(.failure(error))
+                return
+            }
+            completion(.success(data))
+        }
     }
     
-    private func createChatRequest(with question: String) -> URLRequest {
+    func createChatRequest(with question: String) -> URLRequest? {
         let url = URL(string: GPTModel.gpt_3_5_turbo.endpoint)!
         var request = URLRequest(url: url)
-        guard let requestBody = try? JSONSerialization.data(withJSONObject: ["question": question]) else { return }
+        guard let requestBody = try? JSONSerialization.data(withJSONObject: ["question": question]) else { return nil }
         
         request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type:")
         request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
         request.httpBody = requestBody
         return request
