338e4532e0dba32d10135c6e03d25f8a01b09964
SooJi <sokoooool86@gmail.com>
2023-10-23 14:43:18 +0900

    :sparkles: Feat: URLSession의 Mock 생성

--- DilemmaDelish/DilemmaDelish/MockURLSession.swift
+++ DilemmaDelish/DilemmaDelish/MockURLSession.swift
@@ -10,3 +10,18 @@ import Foundation
 protocol URLSessionProtocol {
     func dataTask(request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol
 }
+
+extension URLSession: URLSessionProtocol {
+    func dataTask(request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol {
+        return dataTask(with: request, completionHandler: completion)
+    }
+}
+
+final class MockURLSession: URLSessionProtocol {
+    
+    var onComplete: ((Data?, URLResponse?, Error?) -> Void)!
+    
+    func dataTask(request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol {
+        return MockURLDataTask(completion: onComplete)
+    }
+}
