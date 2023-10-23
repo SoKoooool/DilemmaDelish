d14ec6f1e54c7c34d0f19c278fbd527b2574c1ad
SooJi <sokoooool86@gmail.com>
2023-10-23 17:59:52 +0900

    :sparkles: Feat: GPT 쿼리 모델에 RequestModel 변환 기능 추가

--- DilemmaDelish/DilemmaDelish/ChatQueryModel.swift
+++ DilemmaDelish/DilemmaDelish/ChatQueryModel.swift
@@ -8,6 +8,18 @@
 import Foundation
 
 struct ChatQueryModel {
-    let model: GPTModel
+    let role: Role
     let message: String
+    
+    enum Role: String {
+        case user
+        case assistant
+        case system
+    }
+    
+    func toRequestModel() -> ChatRequestModel {
+        let message = ChatRequestModel.Message(role: role.rawValue, content: message)
+        let model = ChatRequestModel(model: GPTModel.gpt_3_5_turbo.rawValue, messages: [message])
+        return model
+    }
 }
