7209e0542b11cd63d07e692bad5b2d11b7424fcb
SooJi <sokoooool86@gmail.com>
2023-10-23 16:44:34 +0900

    :sparkles: Feat: api 호출에 사용될 쿼리 모델 생성

---
+++ DilemmaDelish/DilemmaDelish/ChatQueryModel.swift
@@ -0,0 +1,13 @@
+//
+//  ChatQueryModel.swift
+//  DilemmaDelish
+//
+//  Created by TORI on 2023/10/23.
+//
+
+import Foundation
+
+struct ChatQueryModel {
+    let model: GPTModel
+    let message: String
+}
