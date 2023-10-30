cd420ea981135aba48ffa98572c8a5356323e076
SooJi <sokoooool86@gmail.com>
2023-10-30 15:12:32 +0900

    :sparkles: Feat: Input 이벤트를 처리하는 옵저버블 생성

--- DilemmaDelish/DilemmaDelish/Pages/CookbookViewModel.swift
+++ DilemmaDelish/DilemmaDelish/Pages/CookbookViewModel.swift
@@ -9,10 +9,26 @@ import Foundation
 import RxSwift
 
 protocol CookbookViewModelProtocol {
-    
+    var fetchRecipes: AnyObserver<Void> { get }
+    var addIngredient: AnyObserver<IngredientItem> { get }
+    var subtractIngredient: AnyObserver<IngredientItem> { get }
 }
 
 final class CookbookViewModel: CookbookViewModelProtocol {
     
     private let disposeBag = DisposeBag()
+    
+    let fetchRecipes: AnyObserver<Void>
+    let addIngredient: AnyObserver<IngredientItem>
+    let subtractIngredient: AnyObserver<IngredientItem>
+    
+    init() {
+        let fetching = PublishSubject<Void>()
+        let adding = PublishSubject<IngredientItem>()
+        let subtracting = PublishSubject<IngredientItem>()
+        
+        fetchRecipes = fetching.asObserver()
+        addIngredient = adding.asObserver()
+        subtractIngredient = subtracting.asObserver()
+    }
 }
