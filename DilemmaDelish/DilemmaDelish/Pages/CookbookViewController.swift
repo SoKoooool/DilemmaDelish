f80bcc1770ed8378f9b559f6dcba6e478d2859b9
SooJi <sokoooool86@gmail.com>
2023-10-30 14:46:20 +0900

    :sparkles: Feat: 뷰모델 생성 및 의존성 주입

--- DilemmaDelish/DilemmaDelish/Pages/CookbookViewController.swift
+++ DilemmaDelish/DilemmaDelish/Pages/CookbookViewController.swift
@@ -9,6 +9,22 @@ import UIKit
 
 final class CookbookViewController: UIViewController {
     
+    // MARK: - Dependecies
+    
+    private let viewModel: CookbookViewModelProtocol
+    
+    // MARK: - Initialization
+    
+    init(viewModel: CookbookViewModelProtocol = CookbookViewModel()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        self.viewModel = CookbookViewModel()
+        super.init(coder: coder)
+    }
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
---
+++ DilemmaDelish/DilemmaDelish/Pages/CookbookViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  CookbookViewModel.swift
+//  DilemmaDelish
+//
+//  Created by TORI on 2023/10/30.
+//
+
+import Foundation
+import RxSwift
+
+protocol CookbookViewModelProtocol {
+    
+}
+
+final class CookbookViewModel: CookbookViewModelProtocol {
+    
+    private let disposeBag = DisposeBag()
+}
