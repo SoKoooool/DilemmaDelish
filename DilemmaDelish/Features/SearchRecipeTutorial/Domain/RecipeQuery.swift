cec55dfbc2f160416a37da48065aa1d21382eb07
SooJi <sokoooool86@gmail.com>
2024-06-10 15:43:21 +0900

    :sparkles: Feat: 레시피 쿼리 모델 정의

---
+++ DilemmaDelish/Features/SearchRecipeTutorial/Domain/RecipeQuery.swift
@@ -0,0 +1,14 @@
+//
+//  RecipeQuery.swift
+//  DilemmaDelish
+//
+//  Created by TORI on 6/10/24.
+//
+
+import Foundation
+
+struct RecipeQuery {
+    let recipeType: String
+    let mainIngredients: [String]
+    let additionalIngredients: [String]
+}
