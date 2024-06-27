ba2b27a159744ed4661b3490c41d4fa422b36cc7
SooJi <sokoooool86@gmail.com>
2024-06-27 14:09:43 +0900

    :sparkles: Feat: 튜토리얼 화면 뷰모델 생성

--- DilemmaDelish/Features/SearchRecipeTutorial/Presentation/ViewModels/RecipeSearchTutorialViewModel.swift
+++ DilemmaDelish/Features/SearchRecipeTutorial/Presentation/ViewModels/RecipeSearchTutorialViewModel.swift
@@ -20,7 +20,35 @@ protocol RecipeSubIngerdientPickerViewModel {
     var subIngredientSelected: AnyObserver<ViewRecipeIngredient> { get }
 }
 
-protocol RecipeSearchTutorialViewModel {
+protocol RecipeSearchTutorialViewModel: RecipeSearchTutorialViewModelType {
     var fetchRecipeDetail: AnyObserver<Void> { get }
     var recipeDetail: Observable<ViewRecipeDetail> { get }
+}
+
+typealias RecipeSearchTutorialViewModelType = RecipeTypePickerViewModel & RecipeMainIngredientPickerViewModel & RecipeSubIngerdientPickerViewModel
+
+final class DefaultRecipeSearchTutorialViewModel: RecipeSearchTutorialViewModel {
+    
+    let fetchRecipeDetail: AnyObserver<Void>
+    let typeSelected: AnyObserver<ViewRecipeType>
+    let mainIngredientSelected: AnyObserver<ViewRecipeIngredient>
+    let subIngredientSelected: AnyObserver<ViewRecipeIngredient>
+    
+    let recipeDetail: Observable<ViewRecipeDetail>
+    
+    init() {
+        let recipeDetailFetching = PublishSubject<Void>()
+        let typeSelecting = PublishSubject<ViewRecipeType>()
+        let mainIngredientSelecting = PublishSubject<ViewRecipeIngredient>()
+        let subIngredientSelecting = PublishSubject<ViewRecipeIngredient>()
+        
+        let fetchedRecipeDetail = PublishSubject<ViewRecipeDetail>()
+        
+        fetchRecipeDetail = recipeDetailFetching.asObserver()
+        typeSelected = typeSelecting.asObserver()
+        mainIngredientSelected = mainIngredientSelecting.asObserver()
+        subIngredientSelected = subIngredientSelecting.asObserver()
+        
+        recipeDetail = fetchedRecipeDetail
+    }
 }
