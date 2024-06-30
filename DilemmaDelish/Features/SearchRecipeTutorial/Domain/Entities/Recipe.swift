3729724c9f202dc8957af7c04272869d788660dc
SooJi <sokoooool86@gmail.com>
2024-06-30 19:46:45 +0900

    :sparkles: Feat: Recipe 엔티티 생성

---
+++ DilemmaDelish/Features/SearchRecipeTutorial/Domain/Entities/Recipe.swift
@@ -0,0 +1,15 @@
+//
+//  Recipe.swift
+//  DilemmaDelish
+//
+//  Created by TORI on 6/30/24.
+//
+
+import Foundation
+
+struct Recipe {
+    let name: String
+    let directions: [String]
+    let mainIngredients: [String]
+    let subIngredients: [String]
+}
