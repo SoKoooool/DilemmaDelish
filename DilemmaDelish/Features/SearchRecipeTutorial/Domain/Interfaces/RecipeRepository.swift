070074d35d96a5999f640b449017fe5774ff2433
SooJi <sokoooool86@gmail.com>
2024-07-07 20:10:24 +0900

    :sparkles: Feat: Recipe Repository 인터페이스 작성

---
+++ DilemmaDelish/Features/SearchRecipeTutorial/Domain/Interfaces/RecipeRepository.swift
@@ -0,0 +1,14 @@
+//
+//  RecipeRepository.swift
+//  DilemmaDelish
+//
+//  Created by TORI on 7/7/24.
+//
+
+import Foundation
+import RxSwift
+
+protocol RecipeRepository {
+    func fetchAllRecipeDetail() -> Observable<Data>
+    func searchAllRecipe(with query: RecipeQuery) -> Observable<Data>
+}
