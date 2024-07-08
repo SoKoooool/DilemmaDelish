95fe3b5b9cf5e92e4bc6e40dddb632cf7c35927a
SooJi <sokoooool86@gmail.com>
2024-07-08 18:30:32 +0900

    :sparkles: Feat: RecipeDetailResponseDTO에 도메인 모델 변환 기능 추가

--- DilemmaDelish/Features/SearchRecipeTutorial/Data/DTO/RecipeDetailResponseDTO.swift
+++ DilemmaDelish/Features/SearchRecipeTutorial/Data/DTO/RecipeDetailResponseDTO.swift
@@ -28,3 +28,29 @@ struct RecipeDetailResponseDTO: Decodable {
         let iconName: String
     }
 }
+
+extension RecipeDetailResponseDTO {
+    func toDomain() -> RecipeDetail {
+        return RecipeDetail(categories: categories.map { $0.toDomain() },
+                            ingredients: ingredients.map { $0.toDomain() },
+                            seasonings: seasonings.map { $0.toDomain() })
+    }
+}
+
+extension RecipeDetailResponseDTO.Category {
+    func toDomain() -> RecipeDetail.Category {
+        return RecipeDetail.Category(iconName: iconName, name: name)
+    }
+}
+
+extension RecipeDetailResponseDTO.Ingredient {
+    func toDomain() -> RecipeDetail.Ingredient {
+        return RecipeDetail.Ingredient(iconName: iconName, name: name, quantity: quantity)
+    }
+}
+
+extension RecipeDetailResponseDTO.Seasoning {
+    func toDomain() -> RecipeDetail.Seasoning {
+        return RecipeDetail.Seasoning(iconName: iconName, name: name)
+    }
+}
