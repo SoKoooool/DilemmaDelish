4546bf05fa934182d921546b7dd5a2c1b49681eb
SooJi <sokoooool86@gmail.com>
2024-07-08 20:04:55 +0900

    :sparkles: Feat: 도메인의 RecipeStore 구현

--- DilemmaDelish/Features/SearchRecipeTutorial/Domain/Interfaces/RecipeRepository.swift
+++ DilemmaDelish/Features/SearchRecipeTutorial/Domain/Interfaces/RecipeRepository.swift
@@ -10,5 +10,5 @@ import RxSwift
 
 protocol RecipeRepository {
     func fetchAllRecipeDetail() -> Observable<Data>
-    func searchAllRecipe(with query: RecipeQuery) -> Observable<Data>
+    func searchAllRecipe(with query: String) -> Observable<Data>
 }
--- DilemmaDelish/Features/SearchRecipeTutorial/Domain/Interfaces/RecipeSearchable.swift
+++ DilemmaDelish/Features/SearchRecipeTutorial/Domain/Interfaces/RecipeSearchable.swift
@@ -12,3 +12,28 @@ protocol RecipeSearchable {
     func fetchRecipeDetail() -> Observable<RecipeDetail>
     func searchRecipe(with query: RecipeQuery) -> Observable<[Recipe]>
 }
+
+final class RecipeStore: RecipeSearchable {
+    
+    private let repository: RecipeRepository
+    private let decoder: JSONDecoder
+    
+    private let disposeBag = DisposeBag()
+    
+    init(repository: RecipeRepository, decoder: JSONDecoder) {
+        self.repository = repository
+        self.decoder = decoder
+    }
+    
+    func fetchRecipeDetail() -> Observable<RecipeDetail> {
+        return repository.fetchAllRecipeDetail()
+            .decode(type: RecipeDetailResponseDTO.self, decoder: decoder)
+            .map { $0.toDomain() }
+    }
+    
+    func searchRecipe(with query: RecipeQuery) -> Observable<[Recipe]> {
+        return repository.searchAllRecipe(with: RecipeRequestDTO(dto: query).query)
+            .decode(type: [RecipeResponseDTO].self, decoder: decoder)
+            .map { $0.map { $0.toDomain() } }
+    }
+}
