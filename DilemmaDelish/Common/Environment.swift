4c47d468196230e54416610a5f1f43e5ebd50cc2
SooJi <sokoooool86@gmail.com>
2024-07-16 20:35:58 +0900

    :sparkles: Feat: 공통 환경 설정 생성

---
+++ DilemmaDelish/Common/Environment.swift
@@ -0,0 +1,24 @@
+//
+//  Environment.swift
+//  DilemmaDelish
+//
+//  Created by TORI on 7/16/24.
+//
+
+import Foundation
+
+struct Environment {
+    static var apiKey: String = {
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "ApiKey") as? String else {
+            fatalError("ApiKey must not be empty in plist")
+        }
+        return apiKey
+    }()
+    
+    static var apiBaseURL: String = {
+        guard let apiBaseURL = Bundle.main.object(forInfoDictionaryKey: "ApiBaseURL") as? String else {
+            fatalError("ApiBaseURL must not be empty in plist")
+        }
+        return apiBaseURL
+    }()
+}
