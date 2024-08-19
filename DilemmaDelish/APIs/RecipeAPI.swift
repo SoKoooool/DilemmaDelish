c71a7399bbd9f2dbd3b6da7119cb05b23eac2489
SooJi <sokoooool86@gmail.com>
2024-08-19 14:18:41 +0900

    :sparkles: Feat: 레시피 관련 API를 처리하는 RecipeAPI 구현

---
+++ DilemmaDelish/APIs/RecipeAPI.swift
@@ -0,0 +1,14 @@
+//
+//  RecipeAPI.swift
+//  DilemmaDelish
+//
+//  Created by TORI on 8/19/24.
+//
+
+import Foundation
+import RxSwift
+
+struct RecipeAPI: APIBase {
+    var baseURL: String
+    var session: URLSession
+}
