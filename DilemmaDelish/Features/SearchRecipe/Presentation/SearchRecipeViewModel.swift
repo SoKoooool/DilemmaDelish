bff91e539ee08961f4f92c04d14f73df6de47881
SooJi <sokoooool86@gmail.com>
2024-09-01 15:16:23 +0900

    :sparkles: Feat: SearchRecipe 뷰모델 구현

--- DilemmaDelish/Features/SearchRecipe/Presentation/SearchRecipeViewModel.swift
+++ DilemmaDelish/Features/SearchRecipe/Presentation/SearchRecipeViewModel.swift
@@ -14,3 +14,37 @@ protocol SearchRecipeViewModel {
     var isLoading: Observable<Bool> { get }
     var errorMessage: Observable<String> { get }
 }
+
+public final class DefaultSearchRecipeViewModel: SearchRecipeViewModel {
+    
+    private let disposeBag = DisposeBag()
+    private let usecase: SearchRecipeUsecase
+    
+    var searchQuery: AnyObserver<String>
+    
+    var recipes: Observable<[Recipe]>
+    var isLoading: Observable<Bool>
+    var errorMessage: Observable<String>
+    
+    init(usecase: SearchRecipeUsecase) {
+        self.usecase = usecase
+        
+        let searching = PublishSubject<String>()
+        let searchResults = PublishSubject<[Recipe]>()
+        let activating = PublishSubject<Bool>()
+        let error = PublishSubject<String>()
+    
+        searchQuery = searching.asObserver()
+        searching
+            .do(onNext: { _ in activating.onNext(true) })
+            .flatMap { usecase.execute(query: $0) }
+            .do(onNext: { _ in activating.onNext(false) })
+            .do(onError: { error.onNext($0.localizedDescription) })
+            .subscribe(onNext: searchResults.onNext)
+            .disposed(by: disposeBag)
+            
+        recipes = searchResults.asObserver()
+        isLoading = activating.asObserver()
+        errorMessage = error.asObserver()
+    }
+}
