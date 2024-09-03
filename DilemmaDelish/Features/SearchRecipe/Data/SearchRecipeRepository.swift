96e4c9e02fd4ca0b1cc5f66dcf1ad79afde09f30
SooJi <sokoooool86@gmail.com>
2024-09-03 12:14:28 +0900

    :sparkles: Feat: toDomain()을 사용하여 fetchRecipe 구현

--- DilemmaDelish/Features/SearchRecipe/Data/SearchRecipeRepository.swift
+++ DilemmaDelish/Features/SearchRecipe/Data/SearchRecipeRepository.swift
@@ -12,4 +12,18 @@ protocol SearchRecipeRepository {
     func fetchRecipe(query: String) -> Observable<[Recipe]>
 }
 
+public final class DefaultSearchRecipeRepository: SearchRecipeRepository {
+    func fetchRecipe(query: String) -> Observable<[Recipe]> {
+        return RecipeAPI().recipe()
+            .map { $0.dto.map { $0.toDomain() } }
+    }
+}
 
+extension RecipeAPI.Recipe.Response.DTO {
+    func toDomain() -> Recipe {
+        return Recipe(name: name,
+                      directions: directions,
+                      ingredients: ingredients,
+                      seasonings: seasonings)
+    }
+}
