d658a53f51b07fa365286b660ac6091296322145
SooJi <sokoooool86@gmail.com>
2024-10-14 13:43:45 +0900

    :wrench: Chore: API 환경변수 수정 및 추가

--- DilemmaDelish/Common/Environment.swift
+++ DilemmaDelish/Common/Environment.swift
@@ -9,14 +9,14 @@ import Foundation
 
 struct Environment {
     static var apiKey: String = {
-        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "ApiKey") as? String else {
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String else {
             fatalError("ApiKey must not be empty in plist")
         }
         return apiKey
     }()
     
     static var apiBaseURL: String = {
-        guard let apiBaseURL = Bundle.main.object(forInfoDictionaryKey: "ApiBaseURL") as? String else {
+        guard let apiBaseURL = Bundle.main.object(forInfoDictionaryKey: "API_BASE_URL") as? String else {
             fatalError("ApiBaseURL must not be empty in plist")
         }
         return apiBaseURL
