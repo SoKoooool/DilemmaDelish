442f21253eb2b6b4a3d8846290f1d86af37404a5
SooJi <sokoooool86@gmail.com>
2024-10-18 15:16:49 +0900

    :sparkles: Feat: 레시피검색 화면진입에 필요한 의존성 등록 및 코디네이터 구현

--- DilemmaDelish/Coordinator/AppCoordinator.swift
+++ DilemmaDelish/Coordinator/AppCoordinator.swift
@@ -24,3 +24,19 @@ public final class AppCoordinator: Coordinator {
         
     }
 }
+
+extension AppCoordinator {
+    func showRecipeSearch() {
+        Container.shared.register(RecipeSearchDIContainer())
+        Container.shared.register(DefaultRecipeSearchCoordinator(navigationController: navigationController))
+        Container.shared.register(DefaultRecipeSearchRepository())
+        Container.shared.register(DefaultRecipeSearchUsecase())
+        Container.shared.register(DefaultRecipeSearchViewModel())
+        Container.shared.register(DefaultRecipeSearchResultsViewModel())
+        Container.shared.register(DefaultRecipeSearchResultDetailViewModel())
+        
+        let coordinator = Container.shared.resolve(DefaultRecipeSearchCoordinator.self)
+        addChildCoordinator(coordinator)
+        coordinator.start()
+    }
+}
