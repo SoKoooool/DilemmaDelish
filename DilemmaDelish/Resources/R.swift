3fbb0b855f77a871897b04180def3bfee5f19aee
SooJi <sokoooool86@gmail.com>
2024-11-13 10:11:29 +0900

    :sparkles: Feat: 리소스 번들 접근 객체 생성

---
+++ DilemmaDelish/Resources/R.swift
@@ -0,0 +1,16 @@
+//
+//  R.swift
+//  DilemmaDelish
+//
+//  Created by TORI on 11/13/24.
+//
+
+import Foundation
+
+public class R {
+    static let bundle = Bundle(for: R.self)
+}
+
+extension R {
+    
+}
