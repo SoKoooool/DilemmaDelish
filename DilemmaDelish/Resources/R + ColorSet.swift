fe110ad4c2a79f52e8563cc70ea54d664c9692c0
SooJi <sokoooool86@gmail.com>
2024-11-14 10:17:49 +0900

    :recycle: Refactor: 컬러셋의 브랜드컬러 명확하게 수정

--- DilemmaDelish/Resources/R + ColorSet.swift
+++ DilemmaDelish/Resources/R + ColorSet.swift
@@ -9,8 +9,12 @@ import UIKit
 
 extension R.ColorSet {
     static var primary: UIColor { .load(name: "Primary_Color") }
-    static var secondary: UIColor { .load(name: "Secondary_Color") }
+    static var primaryLighten: UIColor { .load(name: "Primary_Lighten_Color") }
+    static var primaryDarken: UIColor { .load(name: "Primary_Darken_Color") }
+    static var primaryDisabled: UIColor { .load(name: "Primary_Disabled_Color") }
     static var background: UIColor { .load(name: "Background_Color") }
+    static var backgroundLighten: UIColor { .load(name: "Background_Lighten_Color") }
+    static var backgroundDarken: UIColor { .load(name: "Background_Darken_Color") }
 }
 
 extension R.ColorSet {
