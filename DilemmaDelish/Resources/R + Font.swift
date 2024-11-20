ef976c37c331784ed660a280b4c5a284f98d991f
SooJi <sokoooool86@gmail.com>
2024-11-20 11:04:25 +0900

    :sparkles: Feat: 폰트를 불러오는 기능 작성
    \

--- DilemmaDelish/Resources/R + Font.swift
+++ DilemmaDelish/Resources/R + Font.swift
@@ -10,3 +10,13 @@ import UIKit
 extension R.Font {
     
 }
+
+extension UIFont {
+    static func load(name: String) -> UIFont {
+        guard let font = UIFont(name: name, size: 12) else {
+            assert(false, "\(name) Failed to load font")
+            return UIFont.systemFont(ofSize: 12)
+        }
+        return font
+    }
+}
