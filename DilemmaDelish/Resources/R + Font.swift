77f11b0c17e1a6d4c634a37bf26ae06d847a5f56
SooJi <sokoooool86@gmail.com>
2024-11-20 11:05:38 +0900

    :sparkles: Feat: Pretendard 폰트 사용을 위한 타입 작성

--- DilemmaDelish/Resources/R + Font.swift
+++ DilemmaDelish/Resources/R + Font.swift
@@ -8,7 +8,17 @@
 import UIKit
 
 extension R.Font {
-    
+    enum Pretendard {
+        static var regular: UIFont { .load(name: "PretendardVariable-Regular") }
+        static var thin: UIFont { .load(name: "PretendardVariable-Thin") }
+        static var extraLight: UIFont { .load(name: "PretendardVariable-ExtraLight") }
+        static var light: UIFont { .load(name: "PretendardVariable-Light") }
+        static var medium: UIFont { .load(name: "PretendardVariable-Medium") }
+        static var semibold: UIFont { .load(name: "PretendardVariable-Semibold") }
+        static var bold: UIFont { .load(name: "PretendardVariable-Bold") }
+        static var extraBold: UIFont { .load(name: "PretendardVariable-ExtraBold") }
+        static var black: UIFont { .load(name: "PretendardVariable-Black") }
+    }
 }
 
 extension UIFont {
